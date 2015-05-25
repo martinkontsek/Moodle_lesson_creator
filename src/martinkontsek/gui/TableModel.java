b42e1f523c3aac59ea6e94c6eccb321f7c19cd74
martinkontsek <martinkontsek@gmail.com>
2015-05-25 12:15:00 +0200

    Allow to choose whether or not previous and next buttons are shown.

--- src/martinkontsek/gui/TableModel.java
+++ src/martinkontsek/gui/TableModel.java
@@ -17,7 +17,7 @@ public class TableModel extends AbstractTableModel
 {
 
     private ArrayList<MoodleFile> aMFileList;
-    private String[] aTableTitle = {"Number", "Original name", "Name in Moodle"};
+    private String[] aTableTitle = {"Slide No.", "Original name", "Name in Moodle", "Prev. Btn.", "Next Btn."};
     
     public TableModel(ArrayList<MoodleFile> paMFileList) 
     {
@@ -33,7 +33,7 @@ public class TableModel extends AbstractTableModel
     @Override
     public int getColumnCount() 
     {
-       return 3; 
+       return 5; 
     }
     
     @Override
@@ -54,9 +54,53 @@ public class TableModel extends AbstractTableModel
                 return mf.getSourceFileName();
             case 2:
                 return mf.getResultFileName();
+            case 3:
+                return mf.getPreviousBtn();
+            case 4:
+                return mf.getNextBtn();
             default:
                 return "";
         }
     }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) 
+    {
+        switch(columnIndex)
+        {
+            case 0:
+                return Integer.class;
+            case 3:
+            case 4:
+                return Boolean.class;
+            default:
+                return String.class;
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) 
+    {
+        if(columnIndex == 3 || columnIndex == 4)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) 
+    {
+        MoodleFile mf = aMFileList.get(rowIndex);
+        boolean val = (boolean)aValue;
+        if(columnIndex == 3)
+        {
+            mf.setPreviousBtn(val);
+        } else if(columnIndex == 4)
+        {
+            mf.setNextBtn(val);
+        }
+            
+    }
+    
     
 }
